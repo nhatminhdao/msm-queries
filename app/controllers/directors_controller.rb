15b00196280c320bc91b0a86b370799cee07dd51
nhatminhdao <nhatminhdao@gmail.com>
2024-07-29 19:07:11 +0000

    edit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,10 +6,11 @@ class DirectorsController < ApplicationController
   def show
     director_id = params.fetch("director_id")
 
-
     matching_records = Director.where({ :id => director_id})
 
     @the_director = matching_records.first
+
+    @matching_movies = Movie.where({ :director_id => @the_director.id})
 
     render({ :template => "director_templates/show_details"})
   end
