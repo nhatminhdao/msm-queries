00179290432b4bb3444711d3fb5c2b1eebadf401
nhatminhdao <nhatminhdao@gmail.com>
2024-07-29 18:37:21 +0000

    edit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,5 +1,16 @@
 class DirectorsController < ApplicationController
   def index
-    render({ :template => "director_templates/list" })
+    render({ :template => "director_templates/index" })
+  end
+
+  def show
+    director_id = params.fetch("director_id")
+
+
+    matching_records = Director.where({ :id => director_id})
+
+    @the_director = matching_records.first
+
+    render({ :template => "director_templates/show_details"})
   end
 end
